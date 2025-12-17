6b57495e568e8011bb6f8d29c1e3ae51c5873892
Wassim Belhaj <wassim-belhaj@hotmail.com>
2025-12-17 11:02:07 +0100

    chore: delete learning site and participant resource constants

--- src/util/constants.js
+++
@@ -1,9 +0,0 @@
-export const learningSites = [
-  {name: "codecademy", host: "www.codecademy.com"},
-  {name: "sololearn", host: "www.sololearn.com"},
-  {name: "udemy", host: "www.udemy.com"},
-];
-
-export const participantResource = {name: "codecademy", host: "www.codecademy.com"}
-// export const participantResource = {name: "sololearn", host: "www.sololearn.com"}
-// export const participantResource = {name: "udemy", host: "www.udemy.com"}
