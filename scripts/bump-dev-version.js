94f826a2df5dc098514a4dcc712306f395131f82
Rasmus Alexander Christiansen <54313567+RasmusAChr@users.noreply.github.com>
2026-03-02 12:00:23 +0100

    Update bump-dev-version.js

--- scripts/bump-dev-version.js
+++ scripts/bump-dev-version.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const manifest = require('../public/manifest.json');
-const now = Date.now();
+const now = Math.floor(Date.now() / 10000);
 const base = manifest.version.split('.').slice(0, 3).join('.');
 manifest.version = `${base}.${now}`;
 fs.writeFileSync('./public/manifest.json', JSON.stringify(manifest, null, 2));
