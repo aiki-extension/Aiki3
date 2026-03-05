0dd3c7c76d09a4bd2829cdd67c920507182761bf
Nikolaj Alsøe <nals@itu.dk>
2026-03-05 14:50:20 +0100

    Moved description text out of redundant const
    Co-Authored-By: Rasmus Alexander Christiansen <54313567+RasmusAChr@users.noreply.github.com>

--- src/Pages/Components/Settings/AikiDescription.svelte
+++ src/Pages/Components/Settings/AikiDescription.svelte
@@ -1,8 +1,10 @@
 <script>
   import Container from "./Container.svelte";  
-const infotext = "Aiki steps in when you open a time wasting site you’ve added to the list. It automatically redirects you to an activity site you’ve chosen (for example studying, meditation, or anything else). Time spent on your activity site counts toward your daily goal. When the goal is reached, Aiki stops intervening until the next day."
 </script>
 
 <Container id="site-input-container" headline="What is Aiki?">
-    <p>{infotext}</p>
+    <p>Aiki steps in when you open a time wasting site you’ve added to the list. 
+      It automatically redirects you to an activity site you’ve chosen (for example studying, meditation, or anything else). 
+      Time spent on your activity site counts toward your daily goal. 
+      When the goal is reached, Aiki stops intervening until the next day.</p>
 </Container>
