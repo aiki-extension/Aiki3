8b6112bc410b016c0d01eed544fe0ecbd131852d
Mathias Larsen <mnla@itu.dk>
2026-03-09 15:36:55 +0100

    Removed  Controlledmode
    Removed Controlledmode and variant config aswell as variants from background

--- src/util/variant.js
+++
@@ -1,5 +0,0 @@
-/**
- * Variant configuration.
- * Change this value to switch between "controlled" and "experimental" variants.
- */
-export const AIKI_VARIANT = "experimental"; // Options: "controlled", "experimental"
