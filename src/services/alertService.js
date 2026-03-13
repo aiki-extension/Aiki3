872e19bc6c88eaa662cf05eb75a2e2842f888555
Nikolaj <nals@itu.dk>
2026-03-13 23:15:55 +0100

    Alert message limit.
    Limited the amount of displayed alerts to 2.

--- src/services/alertService.js
+++ src/services/alertService.js
@@ -6,26 +6,34 @@ function createAlertStore() {
 
   return {
     subscribe,
+    
     // Add a new alert
     add: (options) => {
-      // Defaults in case some parameters aren't passed
-      const alert = {
-        id: crypto.randomUUID(),
-        type: 'info',
-        dismissible: true,
-        time: 3000, // 3 seconds default
-        ...options
-      };
+        // Defaults in case some parameters aren't passed
+        const alert = {
+            id: crypto.randomUUID(),
+            type: 'info',
+            dismissible: true,
+            time: 3000, // 3 seconds default
+            ...options
+        };
 
-      // Push the new alert to the store
-      update(alerts => [...alerts, alert]);
+        // Push the new alert to the store
+        update(alerts => {
+            // Ensure that no more than 2 alerts are displayed at once
+            if (alerts.length >= 2 ) {
+                alerts = alerts.slice(1);
+            }
 
-      // If time > 0, set a timer to auto-remove it
-      if (alert.time > 0) {
-        setTimeout(() => {
-          alertStore.remove(alert.id);
-        }, alert.time);
-      }
+            return [...alerts, alert];
+        });
+
+        // If time > 0, set a timer to auto-remove it
+        if (alert.time > 0) {
+            setTimeout(() => {
+            alertStore.remove(alert.id);
+            }, alert.time);
+        }
     },
     
     // Remove a specific alert by ID
