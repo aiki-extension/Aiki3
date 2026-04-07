abac2fff72160f6662c668b76b8de41444aeb769
Rasmus Alexander Christiansen <54313567+RasmusAChr@users.noreply.github.com>
2026-04-07 22:00:33 +0200

    Make redirection site submittable by pressing enter
    Uses form submit for redirection save

--- src/Pages/Components/Settings/SetRedirection.svelte
+++ src/Pages/Components/Settings/SetRedirection.svelte
@@ -103,7 +103,7 @@
 <Container headline="Redirection Settings">
   <h5>Your Redirection Platform:</h5>
   <div class="container">
-    <div class="full" id="learning-url-container">
+    <form class="full" id="learning-url-container" on:submit|preventDefault={saveUri}>
       <input
         class="form-control form-control-lg url-input"
         type="text"
@@ -117,9 +117,8 @@
       <div class="actions">
         {#if isEditing}
           <button
-            type="button"
+            type="submit"
             class="btn btn-success"
-            on:click={saveUri}
             id="learning-url-save"
           >
             Save
@@ -144,7 +143,7 @@
           </button>
         {/if}
       </div>
-    </div>
+    </form>
   </div>
   <hr />
   <TimeSettings {user} />
