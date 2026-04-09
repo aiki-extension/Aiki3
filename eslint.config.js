410adfb4f1ffcc7e0fecf7cfe547d08f80bc725a
DragonFlyersx <Mathiasl2@hotmail.com>
2026-04-09 11:59:57 +0200

    Created a linter for

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import svelte from "eslint-plugin-svelte";
+import svelteParser from "svelte-eslint-parser";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: [".svelte"]
+      }
+    },
+    plugins: {
+      svelte
+    },
+    rules: {
+      ...svelte.configs.recommended.rules
+    }
+  },
+  {
+    files: ["**/*.{js}"],
+    languageOptions: {
+      parser: tsParser
+    },
+    rules: {}
+  },
+  {
+    rules: {
+      "no-unused-vars": "warn"
+    }
+  }
+];
